830cf5c6840882d7c119a633889dd608cb72f3a6
Einswen <trisaer@outlook.com>
2026-06-30 21:00:47 +0800

    完善了后端 添加了账号同步功能 添加了账号注册接口

--- backend/src/main/java/com/smarthome/api/common/GlobalExceptionHandler.java
+++ backend/src/main/java/com/smarthome/api/common/GlobalExceptionHandler.java
@@ -2,12 +2,17 @@ package com.smarthome.api.common;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+  private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
   @ExceptionHandler(ApiException.class)
   public ResponseEntity<ApiError> handleApi(ApiException exception) {
     return ResponseEntity.status(exception.status())
@@ -22,8 +27,15 @@ public class GlobalExceptionHandler {
     return ResponseEntity.badRequest().body(ApiError.of(message, HttpStatus.BAD_REQUEST.value()));
   }
 
+  @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+  public ResponseEntity<ApiError> handleMethodNotSupported(HttpRequestMethodNotSupportedException exception) {
+    return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+        .body(ApiError.of("请求方法不支持", HttpStatus.METHOD_NOT_ALLOWED.value()));
+  }
+
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ApiError> handleUnexpected(Exception exception) {
+    log.error("Unhandled API exception", exception);
     return ResponseEntity.internalServerError()
         .body(ApiError.of("服务暂时不可用", HttpStatus.INTERNAL_SERVER_ERROR.value()));
   }
